583554a85c583ff496df49c1085753058e0c4d14
kwasi <affulnana04@gmail.com>
2025-05-07 13:17:13 +0000

    add the products component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,22 +1,25 @@
 import { ArrowRight } from "lucide-react";
-import { Icon } from "@iconify/react";
+import wallet from "../assets/images/wallet-line.png";
+import futurePlan from "../assets/images/future-plan.png";
+import loans from "../assets/images/loans.png";
+import SME from "../assets/images/SME.png";
 
 const services = [
   {
     title: "Ubuntu Fixed Deposit Investment",
-    icon: "ri:wallet-line",
+    icon: wallet,
   },
   {
     title: "Ubuntu Future Investment Plan",
-    icon: "fluent:data-trending-16-regular",
+    icon: futurePlan,
   },
   {
     title: "Salaried Loans",
-    icon: "ri:money-dollar-circle-line",
+    icon: loans,
   },
   {
     title: "Ubuntu Capital FastSME Loans",
-    icon: "carbon:global-loan-and-trial",
+    icon: SME,
   },
 ];
 
@@ -47,8 +50,13 @@ export default function Products() {
               key={idx}
               className="flex flex-col gap-6 bg-[#A9CAF2]/50 rounded-3xl px-6 py-10 items-start hover:shadow-lg transition duration-300 h-[250px] lg:h-[300px] "
             >
-              <div className="bg-white p-3 rounded-2xl">
-                <Icon icon={service.icon} className="w-8 h-8 text-[#225EA6]" />
+              <div className="bg-white p-4 rounded-2xl">
+                {/* <Icon icon={service.icon} className="w-8 h-8 text-[#225EA6]" /> */}
+                <img
+                  src={service.icon}
+                  alt="icon"
+                  className="w-[1.5rem] h-[1.5rem]"
+                />
               </div>
               <div className="text-xl md:text-2xl font-semibold text-gray-900">
                 {service.title}
