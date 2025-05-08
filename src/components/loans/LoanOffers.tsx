cae7523f957d2f51037e1a26d823711a903244fe
kwasi <affulnana04@gmail.com>
2025-05-08 17:41:07 +0000

    correct typo in loans offers page

--- src/components/loans/LoanOffers.tsx
+++ src/components/loans/LoanOffers.tsx
@@ -5,7 +5,7 @@ function LoanOffers() {
     <div className="bg-[#225EA6]/10 py-16 md:py-24">
       <div className="max-w-7xl mx-auto mb-12 gap-6 px-6 sm:px-12 lg:px-0 flex flex-col md:flex-row md:items-end justify-between">
         <h1 className="text-3xl md:text-4xl max-w-md font-semibold text-gray-900">
-          Need a loan? These are variousloans offers
+          Need a loan? These are various loans offers
         </h1>
       </div>
       <ServiceCarousel />
