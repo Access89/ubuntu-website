caee91477487c07445b28d427c4fd7f5e158ef5b
kwasi <affulnana04@gmail.com>
2025-05-08 14:24:51 +0000

    add ussd to the CTA

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -13,15 +13,20 @@ export default function CallToAction() {
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-l from-[#225EA6]/50 to-[#225EA6]/40 lg:hidden md:rounded-2xl"></div>
-
-      {/* Text and Button */}
-      <div className="w-full lg:w-1/2 text-left z-30 flex flex-col items-center lg:items-start  justify-center ">
-        <h3 className="text-2xl sm:text-3xl lg:text-4xl font-semibold text-white lg:text-[#010101] max-w-[500px] text-center lg:text-left pb-4 lg:pb-8">
-          Open an investment account today and experience next-gen services
-        </h3>
-        <button className="inline-flex items-center px-6 py-3 bg-[#225EA6] border hover:text-[#225EA6] border-[#225EA6] text-white rounded-full text-sm font-medium hover:bg-white transition duration-300">
-          Call Us Now <ArrowRight className="ml-2 w-4 h-4" />
-        </button>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 w-full z-20">
+        {/* Text and Button */}
+        <div className="w-full lg:w-1/2 text-left z-30 flex flex-col items-center lg:items-start  justify-center ">
+          <h3 className="text-2xl sm:text-3xl lg:text4xl font-semibold text-white lg:text-[#010101] max-w-[450px] text-center lg:text-left pb-4 lg:pb-8">
+            Open an investment account today and experience next-gen services
+          </h3>
+          <button className="inline-flex items-center px-6 py-3 bg-[#225EA6] border hover:text-[#225EA6] border-[#225EA6] text-white rounded-full text-sm font-medium hover:bg-white transition duration-300">
+            Call Us Now <ArrowRight className="ml-2 w-4 h-4" />
+          </button>
+        </div>
+        <div className="w-full lg:w-1/2 text-left z-30 flex flex-col items-center justify-center text-5xl uppercase text-white ">
+          <p>ussd code</p>
+          <p>*789*712#</p>
+        </div>
       </div>
     </section>
   );
