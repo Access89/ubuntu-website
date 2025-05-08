1d6d38f3132fb028379b3bda625f86aa4dc8f0af
kwasi <affulnana04@gmail.com>
2025-05-08 15:15:17 +0000

    change the background colour

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,7 +3,7 @@ import ServiceCarousel from "./ServiceCarousel";
 
 export default function Products() {
   return (
-    <section className="bg-[#f5f5f7] py-16 md:py-24">
+    <section className="bg-[#225EA6]/10 py-16 md:py-24">
       {/* Text Section - Similar to Apple's */}
       <div className="max-w-7xl mx-auto mb-12 gap-6  px-6 sm:px-12 lg:px-0 flex flex-col md:flex-row md:items-end justify-between">
         <h1 className="text-3xl md:text-4xl max-w-md font-semibold text-gray-900">
