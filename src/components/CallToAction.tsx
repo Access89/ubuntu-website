5f58e37f0ad06285a1cf67424e3cd4a8bc2b3328
kwasi <affulnana04@gmail.com>
2025-05-08 15:10:32 +0000

    improve the layout of the call to action

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,12 +1,14 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 export default function CallToAction() {
   return (
-    <div className="bg-[#225EA6] text-white rounded-3xl p-6 md:p-8 lg:p-10 lg:px-16 flex flex-col md:flex-row items-start md:items-center justify-between gap-6">
+    <div className="bg-[#225EA6] mx-4 sm:mx-auto sm:w-[75%] lg:w-full text-white rounded-3xl p-6 md:p-8 lg:p-10 lg:px-16 flex flex-col lg:flex-row items-start lg:items-center justify-between gap-6">
       <div className="space-y-2 max-w-3xl">
-        <h3 className="text-[17px] font-semibold mb-4">
+        <h3 className="text-sm lg:text-[17px] font-semibold mb-2">
           Access Loans, Savings & More Instantly
         </h3>
 
-        <p className="text-lg font-semibold leading-snug">
+        <p className=" text-base lg:text-lg font-semibold leading-[#150]">
           Dial <span className="font-bold">*789*712#</span> to manage your
           account, apply for a loan, or check your balance—anytime, anywhere, on
           any phone. No internet? No problem. Ubuntu Capital is just a dial
@@ -15,8 +17,11 @@ export default function CallToAction() {
       </div>
 
       {/* CTA Button */}
-      <button className="border-2 border-white rounded-full px-6 py-2 font-semibold hover:bg-white hover:text-[#120B27] transition-colors">
-        Start now
+      <button className="lg:border-2 border-white rounded-full lg:px-6 lg:py-2 font-semibold hover:bg-white hover:text-[#120B27] transition-colors">
+        <span className="lg:hidden">
+          <Icon icon={"maki:arrow"} />
+        </span>
+        <span className="hidden lg:block">Start now</span>
       </button>
     </div>
   );
