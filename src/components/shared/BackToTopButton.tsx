6d76e01ed6d106ab38109002e80c065bf458a494
kwasi <affulnana04@gmail.com>
2025-05-13 19:19:59 +0000

    show back to top button on only smaller screens

--- src/components/shared/BackToTopButton.tsx
+++ src/components/shared/BackToTopButton.tsx
@@ -26,7 +26,7 @@ function BackToTopButton() {
       onClick={scrollToTop}
       className={`${
         isVisible ? "fixed" : "hidden"
-      } bottom-6 right-6 bg-[#225EA6] text-white hover:text-white p-4 rounded-full shadow-xl hover:bg-[#307bd6] transition z-50`}
+      } hidden md:block bottom-6 right-6 bg-[#225EA6] text-white hover:text-white p-4 rounded-full shadow-xl hover:bg-[#307bd6] transition z-50`}
     >
       <Icon icon="iconoir:fast-arrow-up" className="text-xl font-thin" />
     </button>
