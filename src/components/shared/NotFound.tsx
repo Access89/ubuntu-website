c3d6fb1c34219a25e5ff4e4d658af99e26599e03
kwasi <affulnana04@gmail.com>
2025-05-14 13:02:27 +0000

    make the text larger on larger screens

--- src/components/shared/NotFound.tsx
+++ src/components/shared/NotFound.tsx
@@ -3,7 +3,7 @@ import NotFoundIllustration from "@/assets/images/404-illustration.svg";
 
 function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white px-6 py-12">
+    <div className="min-h-screen flex items-center justify-center bg-white px-6 py-12 2xl:py-0">
       <div className="max-w-2xl text-center">
         <img
           src={NotFoundIllustration}
@@ -11,9 +11,13 @@ function NotFound() {
           className="mx-auto w-96 mb-8"
         />
 
-        <h1 className="text-5xl font-bold text-[#225EA6] mb-4">404</h1>
-        <h2 className="text-2xl font-semibold mb-2">Page Not Found</h2>
-        <p className="text-gray-600 mb-6">
+        <h1 className="text-6xl md:text-8xl font-bold text-[#225EA6] mb-4">
+          404
+        </h1>
+        <h2 className="text-2xl md:text-3xl font-semibold mb-2">
+          Page Not Found
+        </h2>
+        <p className="text-gray-600 mb-6 text-lg md:text-xl">
           Sorry, the page you're looking for doesn’t exist or has been moved.
         </p>
 
